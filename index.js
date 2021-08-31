98009ab2d9bc87bca5113c346809009ad82507fe
Key17 <84156771+Key17@users.noreply.github.com>
2021-08-30 20:46:46 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const togglerButton = document.getElementsByClassName('toggler')[0];
-const navBar = document.getElementsByClassName('togled')[0];
-togglerButton.addEventListener('click', () => {
-    navBar.classList.toggle('active')
-})
