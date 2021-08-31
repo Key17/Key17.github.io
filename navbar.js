240117d52fd5963a1dbe8d06675801a8191b5161
Key17 <84156771+Key17@users.noreply.github.com>
2021-08-30 20:47:35 -0500

    Delete navbar.js

--- navbar.js
+++
@@ -1,5 +0,0 @@
-const togglerButton = document.getElementsByClassName('toggler')[0];
-const navBar = document.getElementsByClassName('toggled')[0];
-togglerButton.addEventListener('click', () =>{
-    navBar.classList.toggle('active');
-})
